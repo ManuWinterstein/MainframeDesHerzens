af62a67be06d65d58acb596e1d84a46d56154f2b
ManuWinterstein <manu.wintersamt@gmail.com>
2025-03-28 05:46:18 +0100

    Rt25-28-3-2

--- js/tools/tools.js
+++ js/tools/tools.js
@@ -13,3 +13,234 @@ function getRanInt(godI){
     return Math.floor( Math.random() * godI )
 
 }
+
+//-------------------------------------------------------------------------getColorBlock------------------------------------
+
+const letters = '0123456789abcdef';
+let color ='#'
+
+function getColor(){  
+    
+    for(let b = 0; b < 3; b++){                 
+
+         let r = Math.floor( Math.random() * 16 )
+
+         color += letters[ r ]
+
+}
+
+return color
+
+}
+
+function getColorRGB( r , g , b ){
+
+    let rC = letters[ Math.floor( Math.random()*16 ) ] + letters[ Math.floor( Math.random()*16 ) ]  
+    let bC = letters[ Math.floor( Math.random()*16 ) ] + letters[ Math.floor( Math.random()*16 ) ]
+    let gC = letters[ Math.floor( Math.random()*16 ) ] + letters[ Math.floor( Math.random()*16 ) ] 
+
+    color += rC + gC + bC
+    
+    if( color == '#000000' || color == '#FFFFFF' ){
+
+        console.log('white', color)
+        getColorRGB( r , g , b )
+
+    }else{
+    
+        return color
+    
+    }
+}
+
+function rgbToGrayHex(r, g, b) {
+
+//const gray = Math.round(0.299 * (255-r) + 0.587 * (255-g) + 0.114 * (255-b))
+const gray = Math.round(0.299 * r + 0.587 * g + 0.114 * b)
+
+const hex = gray.toString(16).padStart(2, '0')
+
+return `${hex}${hex}${hex}`
+
+}
+
+
+
+
+//--------------------------------------------------------------------------getViewBlock------------------------------------
+
+function getViewRGB( tn , canX, canY, ctx , r , g , b ){
+  
+    for( let i = 0; i <= canX; i++ ){
+       
+        for( let j = 0; j <= canY; j++){
+              
+                let int = 0
+
+                ctx.fillStyle = getColorRGB( r , g , b)                       
+                            
+                ctx.fillRect( i , j , 1 , 1 );                         
+        
+        }
+    }
+
+    return ctx
+}
+
+
+function getViewScrabblrdColors( tn , canX, canY, ctx  ){
+
+    const didder = getCharacter( 'didder' )
+    
+    let didScrab
+    let tnscrab
+  
+    for( let i = 0; i <= canX; i++ ){
+       
+        for( let j = 0; j <= canY; j++){
+             
+                let int = 0
+
+                didScrab = getRanInt( didder.scrabble ) % 12
+                    
+                tnScrab  = getRanInt( tn.scrabble ) % 12
+                                                    
+                if( didScrab == 12 && tnScrab == 1 ){
+                            
+                    ctx.fillStyle = getColorRGB()                       
+                            
+                    ctx.fillRect( i , j , 1 , 1 );                         
+        
+                }
+        }
+    }
+
+    return ctx
+}
+
+function drawCanvas( ctx, c , j , ü0, acht, dpi , i ){                    
+
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+     
+    for( let a = 0; a < c.length; a++){
+     
+         for (let b = 0; b < 6; b++) {
+             color += letters[Math.floor(Math.random() * 16)];
+         } 
+
+         ctx.fillStyle = color
+         ctx.fillRect( c[ a ] * ( Math.floor( Math.random() * 30 * ü0 / acht ) ) + 2, j[ a ] * ( Math.floor( Math.random() * 18 * acht ) - 3 ), 15, 40 );
+     
+     }
+
+ }
+
+//-------------------------------------------------------------------------------spellTrigger------------------------------------------------------------
+
+function trigger( spell , pulle , margottel ){
+
+    let tS = 'trigger'
+
+    for( let i = 0; i < 3 ; i++){
+        tS += spell + pulle ? 'mach doch einfach volle pulle' : '' + margottel ? 'ich bin das margottel' : '' + 'trigger'
+    } 
+    tS += spell + ' triggerShit '
+
+    triggerShit( tS , pulle , margottel ).then()
+
+}
+
+function triggerShit( tS , i ){
+
+if( i < 1000000000000 ) {
+     
+    return Promise.resolve().then(triggerShit( tS , i++ )) 
+}
+
+}
+
+//-------------------------------------------------------------------------------getABetFunction-----------------------------------------------
+
+let countDicing = 0
+let countSameDice = 0
+let decUValue = '0'
+let dice = []
+let diceNumber = 0
+let diceNumbers = []
+
+function checkIntegrity( eyes ){
+
+    let sumDices = 0
+
+    for(let i = 0; i < 12; i++){
+
+        let dice = getRanInt( eyes )
+
+        if( dice == 6 ){
+
+            i--
+
+        }
+
+        sumDices += dice
+
+
+
+    }
+
+    return sumDices
+
+}
+
+function dicing( mod ){
+            
+    let uValue = 0
+
+    mod = mod ? mod : 12   
+
+    let m     =  new Date().getMilliseconds() % mod 
+    let t       =  String(new Date().getTime()) 
+    
+    let t1      =   t.slice(t.length - 4 , t.length -3 ) 
+    let t2      =   t.slice(t.length - 3 , t.length -2 ) 
+    let t3      =   t.slice(t.length - 2 , t.length -1 ) 
+
+    countDicing++  
+    
+    uValue = countDicing 
+    
+    if( m == 0 ){
+
+        uValue = String(uValue).slice(String(uValue).length - 3 , String(uValue).length - 1 ) 
+        
+        if(uValue == decUValue){
+
+            countSameDice++ 
+
+        }else{ 
+
+            decUValue = uValue; countSameDice = 0 
+
+        } 
+
+    }
+
+    if( countSameDice == 8 ){
+        
+        diceNumber = countDicing
+
+        diceNumbers.push(( diceNumber + '').slice( 0 , ( '' + diceNumber ).length - 2 ) ) 
+        
+        countDicing = 0
+        countSameDice = 1
+        
+        dice.push(Number( uValue ) )     
+
+    }
+    else{
+
+        dicing()
+
+    }
+}
