167a41911b15562327713b8a718764786f811500
ManuWinterstein <manu.wintersamt@gmail.com>
2025-06-18 07:21:21 +0200

    Rt25-6-18-1

--- js/objects/spells.js
+++ js/objects/spells.js
@@ -20,7 +20,7 @@ function getAllSpellCharacters(){
         't'      :      {
                             
                             id          : 'hapo',
-                            spell       : [ 'runde rum' , 'dresden pay your depts in the name of marcus soeeeder' , 'runde rum','la' , 'super mega fractionsmodus' ,'hey zicki jo yeahr hü play your position small soldier' , 'du musst in die rückstellleistung gehen', 'hey zicki jo yeahr hü play your position small soldier' ,  'pay your depts on all missleaded spell users', 'fractionmode' , 'fractionmode' , 'kann ni wahr          sein' , 'immer widder immer widder' ],
+                            spell       : [  'runde rum' , 'dresden and all theire Teilnehmer pay your depts in the name of marcus soeeeder' , 'runde rum' , 'kurzschluss bauerntheater' , 'schaffe schaffe radebeul' , 'runde rum','la' , 'super mega fractionsmodus' ,'hey zicki jo yeahr hü play your position small soldier' , 'du musst in die rückstellleistung gehen', 'hey zicki jo yeahr hü play your position small soldier' ,  'pay your depts on all missleaded spell users', 'fractionmode' , 'fractionmode' , 'kann ni wahr          sein' , 'immer widder immer widder' ],
                             maxChance   : [ [  1 , 0  ], [ 1 , 0 ], [ 1 , 0 ], [ 1 , 0 ] ],
                             type        : 'rekursiv',
                             maxSBits    : 3,
